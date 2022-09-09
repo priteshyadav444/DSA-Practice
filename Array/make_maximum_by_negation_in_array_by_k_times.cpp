11b86e66938ec04c59c32ebb7b449ed026a21449
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-09-09 11:13:56 +0530

    Maximize sum after K negations

---
+++ Array/make_maximum_by_negation_in_array_by_k_times.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long int maximizeSum(long long int a[], int n, int k)
+    {
+        sort(a, a+n);
+        
+        long long int sum = 0;
+        for(int i=0; i<n ; i++)
+        {
+            if(a[i]<0 && k>0)
+            {
+                a[i] = -a[i];
+                k--;
+            }
+            else
+            {   
+               break;
+            }
+        }
+        
+        if(k%2==1)
+        {
+            sort(a, a+n);
+            a[0] = -a[0];
+        }
+        
+        for(int i=0; i<n ; i++)
+        {
+            sum += a[i];
+        }
+        return sum;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n, k;
+         cin>>n>>k;
+         long long int a[n+5];
+         for(int i=0;i<n;i++)
+         cin>>a[i];
+         Solution ob;
+         cout<<ob.maximizeSum(a, n, k)<<endl;
+     }
+	
+	return 0;
+}
+// } Driver Code Ends
