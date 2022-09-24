a38e0784434e628665f38e56f3b6fccf263b3b0f
priteshyadav444 <priteshyadav2015@Gmail.com>
2022-09-24 18:05:25 +0530

    nth item by adding two element

---
+++ Array/easy/nth_item_through_sum_of_two_array_element.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int nthItem(int n, int m, int A[], int B[], int N)
+    {
+        set <int> s;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                s.insert((A[i]+B[j]));
+            }
+        }
+        
+        int count = 0;
+        for(auto x:s){ count++; if(count==N) return x; }
+        
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int L1, L2, N;
+        cin>>L1>>L2;
+        int A[L1], B[L2];
+        for(int i = 0;i < L1;i++)
+            cin>>A[i];
+        for(int i = 0;i < L2;i++)
+            cin>>B[i];
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.nthItem(L1, L2, A, B, N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
